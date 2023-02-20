75b176cc7d5de729a64cc2ee4ea680fc5ba21ba1
Jamil Salim Daruix <jamilsd@gmail.com>
2023-02-20 14:36:52 -0300

    add example

---
+++ Veiculo-Console/VeiculoExemplo/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using VeiculoExemplo;
+
+Console.WriteLine("Hello, World!");
+
+Veiculo veiculo1 = new Veiculo();
+veiculo1.Id = 1;
+veiculo1.Nome = "Teste 1";
+
+Veiculo veiculo2 = new Veiculo();
+veiculo2.Id = 2;
+veiculo2.Nome = "Teste 2";
+
+Repositorio.Inserir(veiculo1);
+Repositorio.Inserir(veiculo2);
+
+
+veiculo1.Id = 1;
+veiculo1.Nome = "ALTERADO PARA UM";
+
+Repositorio.Alterar(veiculo1);
+
+
+var listaAntesExcluir = Repositorio.Listar();
+
+Repositorio.Remover(1);
+
+var listaApoxExcluir = Repositorio.Listar();
---
+++ Veiculo-Console/VeiculoExemplo/Repositorio.cs
@@ -0,0 +1,42 @@
+﻿namespace VeiculoExemplo
+{
+    public static class Repositorio
+    {
+        private static List<Veiculo> Lista { get; set; }
+
+        static Repositorio()
+        {
+            if (Lista == null)
+            {
+                Lista = new List<Veiculo>();
+            }
+        }
+
+        public static void Inserir(Veiculo veiculo)
+        {
+            Lista.Add(veiculo);
+        }
+
+        public static void Alterar(Veiculo veiculo)
+        {
+            foreach (var item in Lista.Where(w => w.Id == veiculo.Id))
+            {
+                item.Nome = veiculo.Nome;
+            }
+        }
+
+        public static void Remover(int id)
+        {
+            var localVeiculo = Lista.FirstOrDefault(w => w.Id == id);
+            if (localVeiculo != null)
+            {
+                Lista.Remove(localVeiculo);
+            }
+        }
+
+        public static List<Veiculo> Listar()
+        {
+            return Lista;
+        }
+    }
+}
